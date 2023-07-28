cb85d2b085901109933dab228bd796ed0421d67a
Zethearc <dariosce99@gmail.com>
2023-07-27 22:16:41 -0500

    DC: Hola Mundo

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'psico-yt',
+    appId: '1:259317905216:web:55ea9ae2f358fe6312eca5',
+    databaseURL: 'https://psico-yt-default-rtdb.firebaseio.com',
+    storageBucket: 'psico-yt.appspot.com',
+    apiKey: 'AIzaSyAwsbrgDIvjvxAF1DNrQ51lXra0Vt3HOR0',
+    authDomain: 'psico-yt.firebaseapp.com',
+    messagingSenderId: '259317905216',
+    measurementId: 'G-87G2R68LMR',
+  },
+};
